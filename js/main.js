db6c7d63dedd8cdd5a391d0299745081b4f6f1dd
Jazzy <b.jazzy.w@gmail.com>
2015-02-24 21:33:00 -0800

    fixes speech bubble sizing

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@
 $( document ).ready(function() {
     $("#header").arctext({radius: 940});
     var data = { loop: true, in: {effect: 'flipInX', sync: true, shuffle: false}, out: {effect: 'flipOutX', sync: true, shuffle: false} };
-			$('.shifter').textillate(data);
+		$('.shifter').textillate(data);
 });
